18dcd5b008009bcd2f532eaeeb7f001a52c26c64
Marco De Nadai <denadai2@Marcos-MacBook-Air.local>
2014-03-18 21:21:41 +0100

    compressed version + hover cities

---
+++ visualisations/map/data/script.py
@@ -0,0 +1,52 @@
+import json
+import requests
+ 
+def example(latitude, longitude):
+
+	base = "http://open.mapquestapi.com/nominatim/v1/reverse.php?"
+	params = "format=json&lat={lat}&lon={lon}".format(
+	lat=latitude,
+	lon=longitude
+	)
+	url = "{base}{params}".format(base=base, params=params)
+	response = requests.get(url)
+	print latitude, longitude
+	
+	
+	response = response.json()
+	place = []
+
+	if 'city' in response['address']:
+		place.append(response['address']['city'])
+
+	if 'state' in response['address']:
+		place.append(response['address']['state'])
+
+	if 'country' in response['address']:
+		place.append(response['address']['country'])
+	
+	cache[latitude+"_"+longitude] = ', '.join([x for x in place])
+	return cache[latitude+"_"+longitude]
+
+
+json_data=open('locations.json')
+
+data = json.load(json_data)
+json_data.close()
+cache = {}
+
+outArray = []
+
+for row in data['RECORDS']:
+
+	if row["lng"]+"_"+row["lat"] in cache:
+		print "hit"
+		placeName = cache[row["lng"]+"_"+row["lat"]]
+	else:
+		placeName = example(row["lng"],row["lat"])
+
+	outArray.append( (row['Time'], row["lat"], row["lng"], row["number_of_tweets"], placeName) )
+	
+
+with open('compressed.json', 'w') as outfile:
+	json.dump(outArray, outfile)
