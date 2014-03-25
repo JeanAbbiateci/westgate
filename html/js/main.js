16dfa7a0bd5bade70857c35ca5978b651ae2f23f
Marco De Nadai <denadai2@wcw-stud-145-109-13-236.wireless.uva.nl>
2014-03-25 14:24:55 +0100

    key scrolling function

--- html/js/main.js
+++ html/js/main.js
@@ -24,80 +24,57 @@
 })(jQuery);
 
 
-var pageHeight = 0;                         // height of the slides in the page
+var pageHeight = 0;                             // height of the slides in the page
 var pagesList = ["page-intro", "tweet-map", "bubble-page", "network", "word-chart", "photo-gallery"];
-var pastPos = 0;                            // past position for the scrollHandler
-var currentPage = pagesList[0];             // current page ID the user is seeing
+var pastPos = 0;                                // past position for the scrollHandler
+var currentPageIndex = 0;                       // current Pagelist index
+var currentPage = pagesList[currentPageIndex];  // current page ID the user is seeing
+var headerHeight = 71;                          // header height. Should be dinamix => fix!
 
 window.load = (function() {
     var pages = document.querySelectorAll('.page');
-    
+
     function setPagesHeight() {
         pageHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0, 700);
         for (var i = 0; i < pages.length; i++) {
             pages.item(i).style.height = pageHeight + 'px';
         }
     }
-
-    var fixUserScrollPosition = function(top) {
-        var isUserGoingUp = false;
-        if (pastPos > top)
-            isUserGoingUp = true;
-console.log(pastPos+" "+top);
-        var element = null;
-        if (isUserGoingUp){
-            index = selectPage(parseInt(top / pageHeight));
-            console.log("going to" + pagesList[index]);
-            element = $("#" + pagesList[index]);
-        } else{
-            index = selectPage(parseInt(top / pageHeight)+1);
-            console.log("going to" + pagesList[index]);
-            element = $("#" + pagesList[index]);
-        }
-
-        if (element) {
-            $(window).off('scroll');
-
-
-            $("html").stop(true, true).animate({
-                scrollTop: $(element).offset().top
-            }, 1000, function() {
-                // Animation complete.
-                console.log("finished");
-                $(window).scrolled(function() {
-                    var top = $(window).scrollTop();
-                    fixUserScrollPosition(top);
-                });
-                currentPageHandler();
-            });
-        }
-    };
-
     setPagesHeight();
     window.onresize = setPagesHeight;
 
-    $(window).scrolled(function() {
-        var top = $(window).scrollTop();
-        fixUserScrollPosition(top);
+
+    // currentPage fix (for refresh scroll)
+    var top = $(window).scrollTop();
+    currentPageIndex = selectPage(parseInt(top / pageHeight));
+    currentPage = pagesList[currentPageIndex];
+
+    $(document).keyup(function(event) {
+        if (event.which === 38)
+            currentPageIndex = selectPage(currentPageIndex - 1);
+        else if (event.which === 40)
+            currentPageIndex = selectPage(currentPageIndex + 1);
+        console.log(currentPageIndex);
+        if (currentPage !== pagesList[currentPageIndex]) {
+            currentPage = pagesList[currentPageIndex];
+
+            var pos = $("#" + currentPage).offset().top;
+            // If it's not the first one, we need to subtract the header height
+            if (currentPageIndex !== 0)
+                pos -= headerHeight;
+            $("html").stop(true, true).animate({
+                scrollTop: pos
+            }, 1000);
+        }
     });
 
-    function currentPageHandler() {
-        $(window).scroll(function() {
-            var top = $(window).scrollTop();
-            var index = parseInt(top / pageHeight);
-            
-            currentPage = pagesList[selectPage(index)];
-        });
-    }
-    
-    function selectPage(index){
+    function selectPage(index) {
         if (index >= pagesList.length)
-                index = pagesList.length-1;
-            else if (index < 0)
-                index = 0;
+            index = pagesList.length - 1;
+        else if (index < 0)
+            index = 0;
         return index;
     }
-    currentPageHandler();
 
 }());
 
