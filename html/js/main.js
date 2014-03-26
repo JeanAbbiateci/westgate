2769494531edde54c409660407e67caaf02616e4
Marco De Nadai <denadai2@Marcos-MacBook-Air.local>
2014-03-26 21:01:12 +0100

    transitions

--- html/js/main.js
+++ html/js/main.js
@@ -6,7 +6,7 @@ var currentPage = pagesList[currentPageIndex];  // current page ID the user is s
 var headerHeight = 71;                          // header height. Should be dinamix => fix!
 
 //Collect pages id
-$('.page').each(function(i){
+$('.page').each(function(i) {
     var currentID = this.id || i;
     pagesList.push(currentID);
 });
@@ -44,21 +44,19 @@ window.load = (function() {
         if (currentPage !== pagesList[currentPageIndex]) {
             currentPage = pagesList[currentPageIndex];
 
-            //var pos = $("#" + currentPage).offset().top;
-            var pos = pageHeight*currentPageIndex;
+            var pos = pageHeight;
             // If it's not the first one, we need to subtract the header height
-           
-                pos = -(pos);
-            /*if (currentPageIndex !== 0)
-                pos -= headerHeight;*/
-            hide_show_slider();
-            
-            d3.select(".tooltip").style("opacity",0).style("display", "none");
+            if (currentPageIndex !== 0) {
+                pos -= (headerHeight - 23); //Don't ask me why the -23
+            }
+            pos = pos * currentPageIndex;
+            pos = -(pos);
 
-            $("#pages").css("transform", "translate(0,"+pos+"px)");
-            /*$("html, body").stop(true).animate({
-                scrollTop: pos
-            }, 1000);*/
+            hide_show_slider();
+
+            d3.select(".tooltip").style("opacity", 0).style("display", "none");
+
+            $("#pages").css("transform", "translate(0," + pos + "px)");
         }
         return;
     }
@@ -70,40 +68,43 @@ window.load = (function() {
         else if (index < 0)
             index = 0;
         return index;
-        
+
     }
 
-    function hide_show_slider(){
+    function hide_show_slider() {
 
         if (currentPage === "tweet-map" || currentPage === "bubble-page")
-            {d3.select("#slider").transition().duration(200).style("display", "block").style("opacity", 1);
-            }
+        {
+            d3.select("#slider").transition().duration(200).style("display", "block").style("opacity", 1);
+        }
         else
-            {d3.select("#slider").transition().duration(200).style("opacity", 0).style("display", "none");
-            }
+        {
+            d3.select("#slider").transition().duration(200).style("opacity", 0).style("display", "none");
+        }
 
     }
 
-    var submenulengths = [8,4,4,3,2];
+    var submenulengths = [8, 4, 4, 3, 2];
 
     //Build menu
     function makemenu()
     {
         var total = 0;
         var pageindexes = range(0, pagesList.length);
-        for (var i=0;i<submenulengths.length;i++)
-        { 
-            var subm = submenulengths[i]
-            var items = range(0,subm);
-            var currentmenu = d3.select("#menu"+i);
+        for (var i = 0; i < submenulengths.length; i++)
+        {
+            var subm = submenulengths[i];
+            var currentmenu = d3.select("#menu" + i);
             currentmenu.select("ul").selectAll("li")
-            .data(pageindexes.slice(total, total+ subm))
-            .enter()
-            .append("li")
-            .attr("class","dot")
-            .attr("id", function(d){return "dot"+d})
-            .on("click", function(d)
-            {   
+                    .data(pageindexes.slice(total, total + subm))
+                    .enter()
+                    .append("li")
+                    .attr("class", "dot")
+                    .attr("id", function(d) {
+                return "dot" + d;
+            })
+                    .on("click", function(d)
+            {
                 //Remove all color and then color current div
                 currentPageIndex = d;
                 pageChanged();
@@ -118,8 +119,8 @@ window.load = (function() {
 
     function updateMenu()
     {
-        d3.selectAll(".dot").classed("active",false);
-        d3.select("#dot"+currentPageIndex).classed("active",true);
+        d3.selectAll(".dot").classed("active", false);
+        d3.select("#dot" + currentPageIndex).classed("active", true);
     }
     updateMenu();
 
@@ -128,7 +129,7 @@ window.load = (function() {
     function range(start, end)
     {
         var array = new Array();
-        for(var i = start; i < end; i++)
+        for (var i = start; i < end; i++)
         {
             array.push(i);
         }
