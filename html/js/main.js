ecca709e9bd82f3f8937553e2c5cbee876d9b6b3
timuric <timuric@gmail.com>
2014-04-01 21:04:14 +0200

    remove crap

--- html/js/main.js
+++ html/js/main.js
@@ -1,10 +1,8 @@
 var pageHeight = 0;                             // height of the slides in the page
 var pagesList = [];
-var pastPos = 0;                                // past position for the scrollHandler
 var currentPageIndex = 0;                       // current Pagelist index
 var currentPage = pagesList[currentPageIndex];  // current page ID the user is seeing
-var headerHeight = 50;                          // header height. Should be dinamix => fix!
-var keyboardUseCorrection = 23;                 // mha!
+var headerHeight = $('#story-nav').height();    // header height. Should be dinamix => fix!
 
 //Collect pages id
 $('.page').each(function(i) {
@@ -12,195 +10,36 @@ $('.page').each(function(i) {
     pagesList.push(currentID);
 });
 
-$(document).ready(function(){
-    $(this).scrollTop(0);
-});
-
-
-/*
 window.load = (function() {
-    var pages = document.querySelectorAll('.page');
 
-    function setPagesHeight() {
-        pageHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        for (var i = 0; i < pages.length; i++) {
-            pages.item(i).style.height = pageHeight + 'px';
-        }
-    }
-    setPagesHeight();
-    window.onresize = setPagesHeight;
-
-
-    // currentPage fix (for refresh scroll)
-    var top = $(window).scrollTop();
-    currentPageIndex = selectPage(parseInt(top / pageHeight));
-    currentPage = pagesList[currentPageIndex];
-
-    $(document).keyup(function(event) {
-        if (event.which === 38)
-            currentPageIndex = selectPage(currentPageIndex - 1);
-        else if (event.which === 40)
-            currentPageIndex = selectPage(currentPageIndex + 1);
-        pageChanged(keyboardUseCorrection);
-        updateMenu();
-
-    });
-
-    function pageChanged(correction)
-    {
-        if (currentPage !== pagesList[currentPageIndex]) {
-            currentPage = pagesList[currentPageIndex];
-
-            var pos = pageHeight;
-            */
-/*If it's not the first one, we need to subtract the header height
-            if (currentPageIndex !== 0) {
-                pos -= (headerHeight - correction); //Don't ask me why the -23
-            }*//*
-
-            pos = pos * currentPageIndex;
-            pos = -(pos);
-
-            hide_show_slider();
-
-            d3.select(".tooltip").style("opacity", 0).style("display", "none");
-
-            $("#pages").css("transform", "translate(0," + pos + "px)");
-        }
-    }
-
-
-    function selectPage(index) {
-        if (index >= pagesList.length)
-            index = pagesList.length - 1;
-        else if (index < 0)
-            index = 0;
-        return index;
-
-    }
-
-    function hide_show_slider() {
-
-        if (currentPage === "tweet-map" || currentPage === "bubble-page")
-        {
-            d3.select("#slider").transition().duration(200).style("display", "block").style("opacity", 1);
-            if(currentPage === "tweet-map")
-                d3.select("#startstop").style("display", "block");
-            else
-                d3.select("#startstop").style("display", "none");
-        }
-        else
-        {
-            d3.select("#slider").transition().duration(200).style("opacity", 0).style("display", "none");
-        }
-
-    }
-
-    var submenulengths = [8, 4, 4, 3, 2];
-    var menuIDs = [0, 9, 13, 16, 18];
-
-    //Build menu
-    function makemenu()
-    {
-        var total = 1;
-        var pageindexes = range(0, pagesList.length);
-        for (var i = 0; i < submenulengths.length; i++)
-        {
-            d3.select("#menu" + i).selectAll('a').attr("id",'nav_id'+total)
-                .on("click", function(){
-                    id = d3.event.toElement.id
-                    to_id = +id.substring("nav_id".length,id.length)
-                    currentPageIndex = to_id;
-                    pageChanged(headerHeight);
-                    updateMenu();
-                    return false;
-                })
-				.on("mouseover", function() {
-					d3.select(this)
-					.style("color", "#dbe1e7");
-				})
-				.on("mouseout", function() {
-					d3.select(this)
-					.style("color", "#8d94a5");
-				});
-
-            var subm = submenulengths[i];
-            var currentmenu = d3.select("#menu" + i);
-            currentmenu.select("ul").selectAll("li")
-                    .data(pageindexes.slice(total, total + subm))
-                    .enter()
-                    .append("li")
-                    .attr("class", "dot")
-                    .attr("id", function(d) {
-                return "dot" + d;
-            })
-                    .on("click", function(d)
-            {
-                //Remove all color and then color current div
-                currentPageIndex = d;
-                pageChanged(headerHeight);
-                updateMenu();
-                return false;
-            });
-
-            total = total + subm;
-        }
-
-
-    }
-
-    makemenu();
-
-    function updateMenu()
-    {
-        d3.selectAll(".dot").classed("active", false);
-        d3.select("#dot" + currentPageIndex).classed("active", true);
-    }
-    updateMenu();
-
-
-    //Range helper function
-    function range(start, end)
-    {
-        var array = new Array();
-        for (var i = start; i < end; i++)
-        {
-            array.push(i);
-        }
-        return array;
-    }
-
-}());
-
-*/
-
-var PageTransitions = (function() {
-
-        var $main = $( '.pages-container' ),
-        $pages = $main.children( 'div.page' ),
-        $bg = $('#bg-images'),
-        $bgImg = undefined,
-        $iterate = $( '#iterateEffects' ),
-        animcursor = 1,
-        pagesCount = $pages.length,
-        current = 0,
-        isAnimating = false,
-        endCurrPage = false,
-        endNextPage = false,
-        opacityVal = 0.2;
-        animEndEventNames = {
-            'WebkitAnimation' : 'webkitAnimationEnd',
-            'OAnimation' : 'oAnimationEnd',
-            'msAnimation' : 'MSAnimationEnd',
-            'animation' : 'animationend'
-        },
-    // animation end event name
-        animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
-    // support css animations
-        support = Modernizr.cssanimations;
+    var submenulengths = [8, 4, 4, 3, 2], //slides in each chapters
+    $chapters = $('#chapters'),
+    $main = $( '.pages-container' ),
+    $pages = $main.children( 'div.page' ),
+    $bg = $('#bg-images'),
+    $bgImg = undefined,
+    pagesCount = $pages.length,
+    current = 0,
+    isAnimating = false,
+    endCurrPage = false,
+    endNextPage = false,
+    opacityVal = 0.2,
+    animEndEventNames = {
+        'WebkitAnimation' : 'webkitAnimationEnd',
+        'OAnimation' : 'oAnimationEnd',
+        'msAnimation' : 'MSAnimationEnd',
+        'animation' : 'animationend'
+    },
+    animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ], // animation end event name
+    support = Modernizr.cssanimations; // support css animations
 
     function init() {
+        //Create dots
+        $.each(submenulengths, function(ind,val){
+            for(var j = 0; j < val; j++) $chapters.find('li > ul').eq(ind).append('<li class="dot"></li>');
+        });
 
+        //Add backgrounds
         $pages.each( function(z) {
             z = z -100;
             var $page = $( this );
@@ -208,18 +47,20 @@ var PageTransitions = (function() {
             var bg = $page.attr('bg') || "";
             $bg.append('<div style="' + bg + ' ; z-index:'+z+'; opacity:0" class="bg-image gray"></div>');
         } );
+        $bgImg =  $('.bg-image'); //update bg variable
 
+        //Initiate first active page
         $pages.eq( current ).addClass( 'current-page' );
-        $bgImg = $('.bg-image');
         $bgImg.eq(0).animate({opacity:opacityVal}, function(){
             $(this).removeClass('gray');
         });
-    }
 
-    $(window).on('keydown', function(e){
-       if(e.keyCode === 39) nextPage(9, 1)
-       if(e.keyCode === 37) nextPage(10, -1)
-    });
+        //Set arrow keys
+        $(window).on('keydown', function(e){
+            if(e.keyCode === 39) nextPage(9, 1)
+            if(e.keyCode === 37) nextPage(10, -1)
+        });
+    }
 
     function onEndAnimation( $outpage, $inpage ) {
         endCurrPage = false;
@@ -293,7 +134,4 @@ var PageTransitions = (function() {
     }
 
     init();
-
-    return { init : init };
-
 })();
